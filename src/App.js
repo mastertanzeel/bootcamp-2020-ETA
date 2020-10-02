23aceba5e9eb4c11ac00640bbf414b1754c73058
tanzeelsaab <hafiztanzeel999@gmail.com>
2020-10-02 19:24:57 +0500

    bootcamp 2020 expense tracking app with surge automation

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 
-import { GlobalProvider } from "./context/GlobalState";
-
-import "./App.css";
-
-function App() {
-  return (
-    <GlobalProvider>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
+import { GlobalP
     </GlobalProvider>
   );
 }
