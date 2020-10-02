e3c9c8dc657779e1df3199466140932b9266c9c4
tanzeelsaab <hafiztanzeel999@gmail.com>
2020-10-02 19:17:58 +0500

    bootcamp 2020 expense tracking app with surge automation

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (amount == 0) {
+      toast.error("Amount should not be 0!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount,
+      };
+
+      addTransaction(newTransaction);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Description</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter description...."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+           (Negative - expense, Positive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn"><strong>Add transaction</strong></button>
+        <ToastContainer />
+      </form>
+    </div>
+  );
+};
